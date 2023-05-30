f3a9c0419e1162499364d10495c0bdc0f59acf31
beyarkay <boydrkane@gmail.com>
2023-05-30 17:38:08 +0200

    Get schedule and outage endpoints working

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,95 @@
-// use rocket::fs::FileServer;
+#[macro_use]
+extern crate rocket;
 use rocket::serde::json::Json;
 
 mod structs;
+use shuttle_runtime::CustomError;
+use sqlx::{Executor, PgPool};
 use structs::{
+    esp::{MunicipalStatus, Stage, StatusInner, StatusOuter},
+    new::{
+        RawMonthlyShedding, RawPeriodicShedding, RawWeeklyShedding, RecurringOutage,
+        RecurringSchedule, ScheduleId,
+    },
     schedule::PowerOutage,
-    status::{EspStatus, EspStatusOuter, MunicipalStatus, Stage},
 };
 
-#[macro_use]
-extern crate rocket;
+#[get("/area_search/<query>")]
+async fn _area_search(query: String) -> Result<Json<Vec<PowerOutage>>, &'static str> {
+    let _ = query;
+    let url = "https://raw.githubusercontent.com/beyarkay/eskom-calendar/main/area_metadata.yaml";
+    let _text_data = reqwest::get(url)
+        .await
+        .map_err(|_err| "Failed to get area_metadata.yaml")?
+        .text()
+        .await
+        .map_err(|_err| "Failed to get text of area_metadata.yaml")?;
 
-#[get("/schedule/<area_name>")]
-async fn schedule(area_name: String) -> Result<Json<Vec<PowerOutage>>, &'static str> {
+    Ok(Json(vec![]))
+}
+
+#[get("/schedules/<area_name>")]
+async fn schedules(area_name: String) -> Result<Json<RecurringSchedule>, String> {
+    let url = format!(
+        "https://raw.githubusercontent.com/beyarkay/eskom-calendar/main/generated/{area_name}.csv"
+    );
+    let response = reqwest::get(url)
+        .await
+        .map_err(|_err| format!("Failed to get CSV file defining schedules for {area_name}"))?;
+
+    if !response.status().is_success() {
+        return Err(format!("Failed to get CSV file from GitHub: {:?}", response));
+    }
+
+    let text_data = response.text().await.map_err(|_err| {
+        format!("Failed to get text of the CSV file defining schedules for {area_name}")
+    })?;
+
+    let mut reader = csv::Reader::from_reader(text_data.as_bytes());
+    let headers = reader
+        .headers()
+        .map_err(|_err| "Couldn't read headers for CSV file")?;
+    let outages: Vec<RecurringOutage>;
+
+    // Parse the CSV file in a manner that depends on the headers
+    if headers.iter().any(|h| h == "date_of_month") {
+        outages = reader
+            .deserialize::<RawMonthlyShedding>()
+            .map(|res| Into::<RecurringOutage>::into(res.unwrap()))
+            .collect::<Vec<_>>();
+    } else if headers.iter().any(|h| h == "day_of_week") {
+        outages = reader
+            .deserialize::<RawWeeklyShedding>()
+            .map(|res| Into::<RecurringOutage>::into(res.unwrap()))
+            .collect::<Vec<_>>();
+    } else if headers.iter().any(|h| h == "day_of_20_day_cycle") {
+        outages = reader
+            .deserialize::<RawPeriodicShedding>()
+            .map(|res| Into::<RecurringOutage>::into(res.unwrap()))
+            .collect::<Vec<_>>();
+    } else {
+        return Err(format!("Couldn't parse headers {:?}", headers));
+    }
+
+    // TODO actually assign values for id, source, info, last_updated, valid_from, valid_until
+    Ok(Json(RecurringSchedule {
+        id: ScheduleId(0),
+        outages,
+        source: vec![],
+        info: vec![],
+        last_updated: None,
+        valid_from: None,
+        valid_until: None,
+    }))
+}
+
+#[get("/outages/<area_name>")]
+async fn outages(area_name: String) -> Result<Json<Vec<PowerOutage>>, String> {
     let url =
         "https://github.com/beyarkay/eskom-calendar/releases/download/latest/machine_friendly.csv";
     let text_data = reqwest::get(url)
         .await
-        .map_err(|_err| "Failed to get machine_friendly")?
+        .map_err(|_err| "Failed to get machine_friendly.csv that defines the outages")?
         .text()
         .await
         .map_err(|_err| "Failed to get text of machine_friendly.csv")?;
@@ -28,6 +101,10 @@ async fn schedule(area_name: String) -> Result<Json<Vec<PowerOutage>>, &'static 
         .filter(|outage| outage.area_name == area_name)
         .collect();
 
+    if outages.is_empty() {
+        return Err(format!("No areas found that match `{area_name}`"));
+    }
+
     Ok(Json(outages))
 }
 
@@ -37,9 +114,9 @@ fn esp_index() -> &'static str {
 }
 
 #[get("/status")]
-fn esp_status() -> Json<EspStatusOuter> {
-    Json(EspStatusOuter {
-        status: EspStatus {
+fn esp_status() -> Json<StatusOuter> {
+    Json(StatusOuter {
+        status: StatusInner {
             capetown: Some(MunicipalStatus {
                 name: "Cape Town".to_string(),
                 next_stages: vec![
@@ -74,13 +151,24 @@ fn esp_status() -> Json<EspStatusOuter> {
     })
 }
 
+#[derive(Clone)]
+struct MyState {
+    pool: PgPool,
+}
+
 #[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
+async fn rocket(#[shuttle_aws_rds::Postgres()] pool: PgPool) -> shuttle_rocket::ShuttleRocket {
+    eprintln!("Including schema");
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    let state = MyState { pool };
+    eprintln!("Building rocket");
     let rocket = rocket::build()
-        .mount("/", routes![schedule]);
-        // .mount("/csv/", FileServer::from("generated/"))
-        // .mount("/esp/2.0", routes![esp_index, esp_status]);
+        .mount("/v1.0", routes![outages, schedules])
+        // .mount("/esp/2.0", routes![esp_index, esp_status])
+        .manage(state);
 
     Ok(rocket.into())
 }
-
